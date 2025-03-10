5569e69aaf3c975da1a3d572d46966995d9afa3a
Ygal Idan -Linux <Ygal.Idan@gmail.com>
2025-03-10 23:34:23 +0200

    Add file to website

--- web.py
+++ web.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return "<h1>Web Site</h1>"
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5678", debug=True)
+    app.run(host="0.0.0.0", port="6789", debug=True)
